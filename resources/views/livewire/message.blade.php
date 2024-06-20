0c180064d62656c9edc8b69467c763944aa8602f
Zeta Vestia <reza.eduwork@gmail.com>
2024-06-20 11:53:03 +0700

    'update'

--- resources/views/livewire/message.blade.php
+++ resources/views/livewire/message.blade.php
@@ -53,9 +53,23 @@
 }
   </style>
   <div class="container contact-form">
-    {{-- <div class="contact-image">
-        <img src="{{url('/')}}/rocket_contact.png" alt="rocket_contact"/>
-    </div> --}}
+    <div style="margin: 30px 0 45px 0;" class="text-center">
+
+      <div data-appear-animation="fadeInDown" style="background-image: url({{url('base')}}/images/mkiri.png); background-size: cover;" class="text-center displainlinetop plepet appear-animation fadeInDown appear-animation-visible"></div><div data-appear-animation="fadeInDown" style="background-image: url({{url('base')}}/images/mtengah.png);text-transform: uppercase; " class="displainlinetop productselec appear-animation fadeInDown appear-animation-visible">
+          <div data-appear-animation="fadeInDown" class="tempatpaddingtext appear-animation fadeInDown appear-animation-visible" style="text-transform: uppercase;">
+              <div data-appear-animation="fadeInDown" class="displainlinetop appear-animation fadeInDown appear-animation-visible">
+                  <img src="{{url('base')}}/images/titik.png" class="titikselect">
+              </div>
+              <div data-appear-animation="fadeInDown" class="displainlinetop productselec appear-animation fadeInDown appear-animation-visible" style="text-transform: uppercase; font-family:veneer;  color:#ac8b58;">
+                KRITIK SARAN
+              </div>
+              <div class="displainlinetop">
+                  <img data-appear-animation="fadeInDown" src="{{url('base')}}/images/titik.png" class="titikselect appear-animation fadeInDown appear-animation-visible">
+              </div>
+          </div>
+      </div>
+      <div data-appear-animation="fadeInDown" style="background-image: url({{url('base')}}/images/mkanan.png); margin-left:-1px;" class="displainlinetop plepet appear-animation fadeInDown appear-animation-visible"></div>
+    </div>
     <form method="post">
         <h3 style="color: #5f492f;">Kritik Saran</h3>
        <div class="row">
